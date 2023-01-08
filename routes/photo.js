b997596dfcf4701b68bf2dbc63bc95cf5c36dccb
Shane99dt <dushen1999thilaksha@gmail.com>
2023-01-08 18:50:42 +0100

    update image

--- routes/photo.js
+++ routes/photo.js
@@ -1,33 +1,52 @@
-const express = require('express')
-const { upload, directory } = require('../config/multer')
-const { Picture } = require('../models')
-const app = express()
+const express = require("express");
+const { upload, directory } = require("../config/multer");
+const { Picture } = require("../models");
+const app = express();
 
-app.post('/:id', upload.single('picture'), async( req, res) => {
-  try{
+app.post("/:id", upload.single("picture"), async (req, res) => {
+  try {
     const picture = await Picture.create({
       url: `http://localhost:5000/${directory}${req.file.filename}`,
-      UserId: req.params.id
-    })
+      UserId: req.params.id,
+    });
 
-    res.json(picture)
-  }catch(e){
-    res.json(e)
-  }
-})
-
-app.get('/:id', async (req, res) => {
-  try {
-    const picture = await Picture.FindOne({
-      where: {
-        UserId: req.params.id
-      }
-    })
-
-    res.json(picture)
+    res.json(picture);
   } catch (e) {
-    res.json(e)
+    res.json(e);
   }
-})
+});
 
-module.exports = app
+app.get("/:id", async (req, res) => {
+  try {
+    const picture = await Picture.findOne({
+      where: {
+        UserId: req.params.id,
+      },
+    });
+    res.json(picture);
+  } catch (e) {
+    res.json(e);
+  }
+});
+
+app.put("/:id/update", upload.single("picture"), async (req, res) => {
+  try {
+    const picture = await Picture.findOne({
+      where: {
+        UserId: req.params.id,
+      },
+    });
+
+    picture.set({
+      url: `http://localhost:5000/${directory}${req.file.filename}`,
+    });
+
+    await picture.save();
+
+    res.json(picture);
+  } catch (e) {
+    res.json(e);
+  }
+});
+
+module.exports = app;
